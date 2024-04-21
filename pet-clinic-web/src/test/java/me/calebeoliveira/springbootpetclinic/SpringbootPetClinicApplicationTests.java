0a3dfc1801b80bb8f10391dbac6eb07b1d483d64
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-04-21 18:15:54 -0300

    fix test

--- pet-clinic-web/src/test/java/me/calebeoliveira/springbootpetclinic/SpringbootPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/me/calebeoliveira/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -1,9 +1,15 @@
 package me.calebeoliveira.springbootpetclinic;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootPetClinicApplicationTests {
+public class SpringbootPetClinicApplicationTests {
 
-
+    @Test
+    public void contextLoads() {
+    }
 }
