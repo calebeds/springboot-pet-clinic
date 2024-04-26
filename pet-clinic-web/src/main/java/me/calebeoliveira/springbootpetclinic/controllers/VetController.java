25ad49b33312cf3ce24a65a20f55aa18792be5c9
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-04-26 16:10:49 -0300

    add vet list and update controller - close #15

--- pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/controllers/VetController.java
@@ -1,12 +1,23 @@
 package me.calebeoliveira.springbootpetclinic.controllers;
 
+import me.calebeoliveira.springbootpetclinic.services.VetService;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
