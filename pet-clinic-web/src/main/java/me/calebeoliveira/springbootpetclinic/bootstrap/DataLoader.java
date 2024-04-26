960e22428701bbb6f3eac9d0d3ee1199e9658eb4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-04-26 16:34:43 -0300

    refactor map impl to abstract ID generation - close #23

--- pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/bootstrap/DataLoader.java
@@ -23,7 +23,6 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
@@ -39,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
