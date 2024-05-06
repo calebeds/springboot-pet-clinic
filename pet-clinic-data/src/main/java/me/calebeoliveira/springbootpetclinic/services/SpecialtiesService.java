2ac481e79f8060450af196afd458f7194832806a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-06 14:36:58 -0300

    add specialties map service impl - close #31

---
+++ pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package me.calebeoliveira.springbootpetclinic.services;
+
+import me.calebeoliveira.springbootpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+ }
