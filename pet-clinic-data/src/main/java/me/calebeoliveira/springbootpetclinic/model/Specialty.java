66541fa06e735d010bc87479ec18d6f108c00926
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-10 15:01:25 -0300

    convert Vets to jpa entities - close #37

--- pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package me.calebeoliveira.springbootpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
