8c65df46e8ad66e686b958782ae056eec65bfc92
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-12 16:54:12 -0300

    implement map impl for visit service - close #46

---
+++ pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package me.calebeoliveira.springbootpetclinic.services;
+
+import me.calebeoliveira.springbootpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
