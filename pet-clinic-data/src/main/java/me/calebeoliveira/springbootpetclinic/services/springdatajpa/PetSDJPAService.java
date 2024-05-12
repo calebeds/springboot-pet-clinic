5994340001a070a66f1a9d92e77273cfd3a52654
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-12 15:45:23 -0300

    add spring data jpa service for pets - close #43

---
+++ pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/services/springdatajpa/PetSDJPAService.java
@@ -0,0 +1,48 @@
+package me.calebeoliveira.springbootpetclinic.services.springdatajpa;
+
+import me.calebeoliveira.springbootpetclinic.model.Pet;
+import me.calebeoliveira.springbootpetclinic.repositories.PetRepository;
+import me.calebeoliveira.springbootpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
