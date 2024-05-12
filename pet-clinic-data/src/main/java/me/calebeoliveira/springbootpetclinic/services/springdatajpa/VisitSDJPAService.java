5f44727b650d793960212f479aeef99335992c07
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-12 17:31:32 -0300

    implement JPA visit service, add loader data - close #47

---
+++ pet-clinic-data/src/main/java/me/calebeoliveira/springbootpetclinic/services/springdatajpa/VisitSDJPAService.java
@@ -0,0 +1,49 @@
+package me.calebeoliveira.springbootpetclinic.services.springdatajpa;
+
+import me.calebeoliveira.springbootpetclinic.model.Visit;
+import me.calebeoliveira.springbootpetclinic.repositories.VisitRepository;
+import me.calebeoliveira.springbootpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
--- pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package me.calebeoliveira.springbootpetclinic.bootstrap;
 
 import me.calebeoliveira.springbootpetclinic.model.*;
-import me.calebeoliveira.springbootpetclinic.services.OwnerService;
-import me.calebeoliveira.springbootpetclinic.services.PetTypeService;
-import me.calebeoliveira.springbootpetclinic.services.SpecialtyService;
-import me.calebeoliveira.springbootpetclinic.services.VetService;
+import me.calebeoliveira.springbootpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +89,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
