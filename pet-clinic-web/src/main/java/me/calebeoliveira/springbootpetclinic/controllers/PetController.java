a0caea3ead2d2bba72c99f423f34ff064bd88cd5
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-07-29 21:45:54 -0300

    add model attribute for Pet Controller - close #56

---
+++ pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/controllers/PetController.java
@@ -0,0 +1,45 @@
+package me.calebeoliveira.springbootpetclinic.controllers;
+
+import me.calebeoliveira.springbootpetclinic.model.Owner;
+import me.calebeoliveira.springbootpetclinic.model.PetType;
+import me.calebeoliveira.springbootpetclinic.services.OwnerService;
+import me.calebeoliveira.springbootpetclinic.services.PetService;
+import me.calebeoliveira.springbootpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+    private static final String VIEWS_PET_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
