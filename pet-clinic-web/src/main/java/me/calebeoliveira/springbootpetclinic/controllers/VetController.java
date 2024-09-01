ffc5b1ad4f5029852dc5bf5a3077806a00968a5c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-01 16:29:55 -0300

    implement vets json endpoint - close #70

--- pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/me/calebeoliveira/springbootpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package me.calebeoliveira.springbootpetclinic.controllers;
 
+import me.calebeoliveira.springbootpetclinic.model.Vet;
 import me.calebeoliveira.springbootpetclinic.services.VetService;
 import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -15,10 +20,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
